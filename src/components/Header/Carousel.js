aa47221734d33915b61b86734dfd96043dc5d918
ianodad <ianodad@gmail.com>
2020-01-24 18:23:47 +0300

    carousel changes

--- src/components/Header/Carousel.js
+++ src/components/Header/Carousel.js
@@ -1,12 +1,5 @@
 import React from 'react'
-// import Slider from '@farbenmeer/react-spring-slider';
-import AwesomeSlider from 'react-awesome-slider';
 
-// // react awsome slider
-// import withAutoplay from 'react-awesome-slider/dist/autoplay';
-// import 'react-awesome-slider/dist/styles.css';
-
-// const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
@@ -16,51 +9,26 @@ import Natural2 from "../../img/Header/natural 4.jpg"
 import Natural3 from "../../img/Header/natural 5.jpg"
 
 
-// const slider = () => (
-{/* <AutoplaySlider
-    play={true}
-    cancelOnInteraction={false}
-    interval={6000}
-    fillParent={true}
->
-    <div style={{ 'height': '200px' }} data-src={require("../../img/Header/natural.jpg")} />
-    <div data-src={require("../../img/Header/natural 1.jpg")} />
-    <div data-src={require("../../img/Header/natural 4.jpg")} />
-    <div data-src={require("../../img/Header/natural 5.jpg")} />
-</AutoplaySlider>
-// ); */}
 
-// const slider = (
-//     <Slider>
-//         <div>
-//             <img src={Natural} alt="natural" />
-//         </div>
-//         <div>
-//             <img src={Natural1} alt="natural" />
-//         </div>
-//         <div>
-//             <img src={Natural2} alt="natural" />
-//         </div>
-//         <div>
-//             <img src={Natural3} alt="natural" />
-//         </div>
-//     </Slider>
-// );
-
+// create alt for image
 
 function Carouselslider() {
     return (
         <Carousel className="m-0" autoPlay showThumbs={false} showStatus={false} dynamicHeight showArrows={false} style={{ height: "auto" }}>
             <div>
-                <img className="carouselImg" src={Natural} style={{}} />
+                <img className="carouselImg" src={Natural} alt="natural" />
                 <p className="legend">Legend 1</p>
             </div>
             <div>
-                <img className="carouselImg" src={Natural1} />
+                <img className="carouselImg" src={Natural1} alt="natural1" />
                 <p className="legend" >Legend 2</p>
             </div>
             <div>
-                <img className="carouselImg" src={Natural2} />
+                <img className="carouselImg" src={Natural2} alt="natural2" />
+                <p className="legend" >Legend 3</p>
+            </div>
+            <div>
+                <img className="carouselImg" src={Natural3} alt="natural3" />
                 <p className="legend" >Legend 3</p>
             </div>
         </Carousel>
